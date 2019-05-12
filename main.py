28b429b4eb0a0597293ff2bd0a223895fb5960e7
moonhyuk <godmoon00@naver.com>
2019-05-12 23:23:31 +0900

    네트워크 모니터링 베이스 코드 구현

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print('hello world')
+import os
+import socket
+from datetime import datetime
+
+
+def get_hosts():
+    return os.environ['hosts'].split(',')
+
+
+def parse_host(host):
+    split_port = host.split(':')
+
+    if len(split_port) > 2 or len(split_port) < 1:
+        raise ValueError('Invalid host')
+
+    elif len(split_port) == 1:
+        split_port.append('80')
+
+    return tuple(split_port)
+
+
+def tcp_open(host):
+    (ip, port) = parse_host(host)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    try:
+        s.connect((ip, int(port)))
+        s.shutdown(socket.SHUT_RDWR)
+        return True
+    except:
+        return False
+    finally:
+        s.close()
+
+
+hosts = get_hosts()
+
+for host in hosts:
+    time = datetime.now()
+
+    if tcp_open(host):
+        print('{}: {} is open'.format(time, host))
+    else:
+        print('{}: {} is not open'.format(time, host))
